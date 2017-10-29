dad26129b105d52fcdc070ef928069387d0ebf3e
Mihai Bujanca <mihaibujanca@yahoo.com>
2017-10-29 04:08:41 +0000

    Remove hardcoded paths, improve cmake files.

--- tests/warp_test.cpp
+++ tests/warp_test.cpp
@@ -12,7 +12,6 @@
 #include "mLibCore.cpp"
 #include "mLibLodePNG.cpp"
 
-
 TEST(WARP_FIELD_TEST, EnergyDataSingleVertexTest)
 {
     const float max_error = 1e-3;
@@ -48,6 +47,7 @@ TEST(WARP_FIELD_TEST, EnergyDataSingleVertexTest)
     params.linearIter = 250;
     params.useOpt = false;
     params.useOptLM = true;
+    params.earlyOut = true;
 
     kfusion::WarpFieldOptimiser optimiser(&warp_field, params);
 
@@ -120,6 +120,7 @@ TEST(WARP_FIELD_TEST, EnergyDataRigidTest)
     params.linearIter = 250;
     params.useOpt = false;
     params.useOptLM = true;
+    params.earlyOut = true;
 
     kfusion::WarpFieldOptimiser optimiser(&warp_field, params);
 
@@ -230,7 +231,7 @@ TEST(WARP_FIELD_TEST, WarpAndReverseTest)
     for(int i = 0; i < warp_field.getNodes()->size(); i++)
     {
         auto t = warp_field.getNodes()->at(i).transform.getTranslation();
-        sum = sum - t;
+        sum = sum + t;
         std::cout<< t <<std::endl;
     }
     ASSERT_NEAR(sum.x_, 0, max_error);
@@ -239,3 +240,151 @@ TEST(WARP_FIELD_TEST, WarpAndReverseTest)
     std::cout<<std::endl<<std::endl<<"Final sum:"<<sum<<std::endl;
 }
 
+TEST(WARP_FIELD_TEST, MultipleNodesTest)
+{
+    const float max_error = 1e-3;
+
+    kfusion::WarpField warp_field;
+    std::vector<cv::Vec3f> warp_init;
+
+    warp_init.emplace_back(cv::Vec3f(1,1,1));
+    warp_init.emplace_back(cv::Vec3f(1,2,-1));
+    warp_init.emplace_back(cv::Vec3f(1,-2,1));
+    warp_init.emplace_back(cv::Vec3f(1,-1,-1));
+    warp_init.emplace_back(cv::Vec3f(-1,1,5));
+    warp_init.emplace_back(cv::Vec3f(-1,1,-1));
+    warp_init.emplace_back(cv::Vec3f(-1,-1,1));
+    warp_init.emplace_back(cv::Vec3f(-1,-1,-1));
+    warp_init.emplace_back(cv::Vec3f(2,-3,-1));
+    warp_init.emplace_back(cv::Vec3f(-3,-3,-2));
+    warp_init.emplace_back(cv::Vec3f(2,-3,3));
+    warp_init.emplace_back(cv::Vec3f(2,2,4));
+    warp_field.init(warp_init);
+
+    std::vector<cv::Vec3f> source_vertices;
+    source_vertices.emplace_back(cv::Vec3f(-3,-3,-3));
+    source_vertices.emplace_back(cv::Vec3f(-2,-2,-2));
+    source_vertices.emplace_back(cv::Vec3f(0,0,0));
+    source_vertices.emplace_back(cv::Vec3f(2,2,2));
+    source_vertices.emplace_back(cv::Vec3f(3,3,3));
+    source_vertices.emplace_back(cv::Vec3f(3,3,3));
+
+    std::vector<cv::Vec3f> canonical_normals;
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+
+    std::vector<cv::Vec3f> target_vertices;
+    target_vertices.emplace_back(cv::Vec3f(-2.95f,-2.95f,-2.95f));
+    target_vertices.emplace_back(cv::Vec3f(-1.95f,-1.95f,-1.95f));
+    target_vertices.emplace_back(cv::Vec3f(0.1,0.1,0.1));
+    target_vertices.emplace_back(cv::Vec3f(2,2,2));
+    target_vertices.emplace_back(cv::Vec3f(3.05,3.05,3.05));
+    target_vertices.emplace_back(cv::Vec3f(3.05,3.05,3.05));
+
+    std::vector<cv::Vec3f> target_normals;
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+
+
+    std::vector<cv::Vec3f> initial_source_vertices(source_vertices);
+    std::vector<cv::Vec3f> initial_source_normals(canonical_normals);
+    CombinedSolverParameters params;
+    params.numIter = 20;
+    params.nonLinearIter = 15;
+    params.linearIter = 250;
+    params.useOpt = false;
+    params.useOptLM = true;
+
+    kfusion::WarpFieldOptimiser optimiser(&warp_field, params);
+    optimiser.optimiseWarpData(source_vertices, canonical_normals, target_vertices, target_normals);
+    warp_field.warp(source_vertices, canonical_normals);
+
+    for(size_t i = 0; i < source_vertices.size(); i++)
+    {
+        ASSERT_NEAR(source_vertices[i][0], target_vertices[i][0], max_error);
+        ASSERT_NEAR(source_vertices[i][1], target_vertices[i][1], max_error);
+        ASSERT_NEAR(source_vertices[i][2], target_vertices[i][2], max_error);
+    }
+}
+
+
+
+TEST(WARP_FIELD_TEST, NonRigidTest)
+{
+    const float max_error = 1e-3;
+
+    kfusion::WarpField warp_field;
+    std::vector<cv::Vec3f> warp_init;
+
+    warp_init.emplace_back(cv::Vec3f(1,1,1));
+    warp_init.emplace_back(cv::Vec3f(1,2,-1));
+    warp_init.emplace_back(cv::Vec3f(1,-2,1));
+    warp_init.emplace_back(cv::Vec3f(1,-1,-1));
+    warp_init.emplace_back(cv::Vec3f(-1,1,5));
+    warp_init.emplace_back(cv::Vec3f(-1,1,-1));
+    warp_init.emplace_back(cv::Vec3f(-1,-1,1));
+    warp_init.emplace_back(cv::Vec3f(-1,-1,-1));
+    warp_init.emplace_back(cv::Vec3f(2,-3,-1));
+    warp_field.init(warp_init);
+
+    std::vector<cv::Vec3f> source_vertices;
+    source_vertices.emplace_back(cv::Vec3f(-3,-3,-3));
+    source_vertices.emplace_back(cv::Vec3f(-2,-2,-2));
+    source_vertices.emplace_back(cv::Vec3f(0,0,0));
+    source_vertices.emplace_back(cv::Vec3f(2,2,2));
+    source_vertices.emplace_back(cv::Vec3f(3,3,3));
+    source_vertices.emplace_back(cv::Vec3f(3,3,3));
+
+    std::vector<cv::Vec3f> canonical_normals;
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+    canonical_normals.emplace_back(cv::Vec3f(0,0,1));
+
+    std::vector<cv::Vec3f> target_vertices;
+    target_vertices.emplace_back(cv::Vec3f(-2.95f,-2.95f,-2.95f));
+    target_vertices.emplace_back(cv::Vec3f(-1.95f,-1.95f,-1.95f));
+    target_vertices.emplace_back(cv::Vec3f(0.1,0.1,0.1));
+    target_vertices.emplace_back(cv::Vec3f(2,2,2));
+    target_vertices.emplace_back(cv::Vec3f(3.05,3.05,3.05));
+    target_vertices.emplace_back(cv::Vec3f(3.05,3.05,3.05));
+
+    std::vector<cv::Vec3f> target_normals;
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+    target_normals.emplace_back(cv::Vec3f(0,0,1));
+
+
+    std::vector<cv::Vec3f> initial_source_vertices(source_vertices);
+    std::vector<cv::Vec3f> initial_source_normals(canonical_normals);
+    CombinedSolverParameters params;
+    params.numIter = 20;
+    params.nonLinearIter = 15;
+    params.linearIter = 250;
+    params.useOpt = false;
+    params.useOptLM = true;
+
+    kfusion::WarpFieldOptimiser optimiser(&warp_field, params);
+    optimiser.optimiseWarpData(source_vertices, canonical_normals, target_vertices, target_normals);
+    warp_field.warp(source_vertices, canonical_normals);
+
+    for(size_t i = 0; i < source_vertices.size(); i++)
+    {
+        ASSERT_NEAR(source_vertices[i][0], target_vertices[i][0], max_error);
+        ASSERT_NEAR(source_vertices[i][1], target_vertices[i][1], max_error);
+        ASSERT_NEAR(source_vertices[i][2], target_vertices[i][2], max_error);
+    }
+}
